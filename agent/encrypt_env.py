9b3d902af4ac69fbe9be5c4c28ae157df8d50450
bdutta-c4 <bhaskar.dutta@c-4analytics.com>
2025-06-24 23:42:19 -0500

    prod promotion: app_environment is now a passed in secret

--- agent/encrypt_env.py
+++ agent/encrypt_env.py
@@ -7,27 +7,48 @@ import argparse, base64, os, sys, secrets
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC   # PBKDF2-HMAC-SHA256
 from cryptography.hazmat.primitives import hashes
 from cryptography.fernet import Fernet
+import boto3
+import json
+import os
+
 
 def derive_key(passphrase: str, salt: bytes, iterations: int = 100_000) -> bytes:
     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt,
                      iterations=iterations)
     return base64.urlsafe_b64encode(kdf.derive(passphrase.encode()))
 
-def main(path_in: str, path_out: str, passphrase: str):
+def main(env: str, path_out: str, passphrase: str):
+    secret_name = f"C-4analytics/fusion/{env}"
+    aws_region = os.environ.get("SECRETS_AWS_REGION", "us-east-1")
+
+    try:
+        session = boto3.session.Session()
+        client = session.client(service_name='secretsmanager', region_name=aws_region)
+        secret_value = client.get_secret_value(SecretId=secret_name)
+        env_secrets = json.loads(secret_value['SecretString'])
+    except Exception as e:
+        raise RuntimeError(f"Failed to obtain AWS variables for {env} environment in region {aws_region}: {e}") from e
+
+    with open("envfile.txt","w") as f:
+        for key, value in env_secrets.items():
+            if isinstance(value, (str, int, float, bool)):
+                if key!="SNOWFLAKE_PRIVATE_KEY":
+                    envln = key+"="+value
+                    f.write(envln + "\n")
+
     salt = secrets.token_bytes(16)                 # 128-bit salt
     key  = derive_key(passphrase, salt)
-    data = open(path_in, "rb").read()
+    data = open("envfile.txt", "rb").read()
     cipher = Fernet(key).encrypt(data)
     with open(path_out, "wb") as f:
         f.write(base64.b64encode(salt) + b"\n" + cipher)
-    print(f"Encrypted {path_in} → {path_out}  (keep passphrase & salt safe!)")
+    print(f"Encrypted {env} → {path_out}  (keep passphrase & salt safe!)")
+    os.remove("envfile.txt")
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument("--env", default=".env")
+    p.add_argument("--env", default="dev")
     p.add_argument("--out", default=".env.enc")
     p.add_argument("--passphrase", required=True)
     a = p.parse_args()
-    if not os.path.exists(a.env):
-        sys.exit(f"{a.env} not found")
     main(a.env, a.out, a.passphrase)

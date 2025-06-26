62009b994da60204c392f7d3a816a31e833d2b7c
bdutta-c4 <bhaskar.dutta@c-4analytics.com>
2025-06-25 23:18:56 -0500

    fixed env file error

--- agent/prompt_executor.py
+++ agent/prompt_executor.py
@@ -10,9 +10,8 @@ import logging
 
 LOG_FILE = 'FusionPromptExecutor.log'
 
-load_dotenv("./agent/.env", override=True)
-from  agent.environments import load_environment
-load_environment()
+from  agent.environments import load_encrypted_variables
+load_encrypted_variables()
 
 # Configure the logging system
 logging.basicConfig(
